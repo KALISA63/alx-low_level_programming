0c7fc08ff5f2d78d0b1396ab6dc43ab18359085f
KALISA63 <kaljama63@gmail.com>
2022-07-02 12:08:03 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Prints all single digit number of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for num (num = 0; num < 10; num++)
+		printf("%d", num);
+			printf("\n");
+			return (0);
+}
