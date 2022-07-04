3db772d82ad7ad3f1f2704a9a6224f5cc5689714
KALISA63 <kaljama63@gmail.com>
2022-07-04 11:38:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <unisdt.h>
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return: Always 1 (Success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2,str1, 59);
+	write(2, str1, 59);
 	return (1);
 }
