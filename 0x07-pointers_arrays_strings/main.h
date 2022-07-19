60b217d361b28030d37ef2ccdc503e1aca283250
KALISA63 <kaljama63@gmail.com>
2022-07-19 07:45:26 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,4 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -8,6 +6,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-int _putchar(char c);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
